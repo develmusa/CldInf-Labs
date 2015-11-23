e57ef7f4e33e1d3841400fa6f8a9e8ad897fbf62
Giorgio Vincenti <giovin@VIN-NB02-2.local>
2015-11-23 17:59:04 +0100

    anpassung ex1

--- Lab10/scripts/simple_hub.py
+++ Lab10/scripts/simple_hub.py
@@ -22,6 +22,7 @@ class SimpleHub(app_manager.RyuApp):
                 match = parser.OFPMatch()
                 actions = [parser.OFPActionOutput (ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
                 self.add_flow(datapath, 0, match, actions)
+
         def add_flow(self, datapath, priority, match, actions):
                 ofproto = datapath.ofproto
                 parser = datapath.ofproto_parser
@@ -36,35 +37,15 @@ class SimpleHub(app_manager.RyuApp):
                 ofproto = datapath.ofproto
                 parser = datapath.ofproto_parser
                 in_port = msg.match['in_port']
-
-                pkt = packet.Packet(msg.data)
-                eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-                dst = eth.dst
-                src = eth.src
-
-                dpid = datapath.id
-                self.mac_to_port.setdefault(dpid, {})
-
-                self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
- 
-                self.mac_to_port[dpid][src] = in_port
-                
-                if dst in self.mac_to_port[dpid]:
-                        out_port = self.mac_to_port[dpid][dst]
-                else:
-                        out_port = ofproto.OFPP_FLOOD
-
+                out_port = ofproto.OFPP_FLOOD
                 actions = [parser.OFPActionOutput(out_port)]
-
-                if out_port != ofproto.OFPP_FLOOD:
-                        match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-                        self.add_flow(datapath, 1, match, actions)
 
                 data = None
                 if msg.buffer_id == ofproto.OFP_NO_BUFFER:
                         data = msg.data
 
-                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                                in_port=in_port, actions=actions, data=data)
+
+                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,in_port=in_port, actions=actions, data=data)
                 datapath.send_msg(out)
+                dpid = datapath.id
+                self.logger.info(("packet in %s %s", dpid, in_port)
